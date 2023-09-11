e4029b14c151da5fce2db08e5bc03a33ef5ca39e
manojgarnayak <manojgarnayak07@gmail.com>
2023-09-11 15:29:00 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all js code as newer version
+
+// alert("hello") we are using nodejs ,not browser
+
+let name = "manoj"
+let age = 23
+let isLoggedIn = true
+
+//null standalone value
+//undefined not defined any value 
+//symbol => unique
+
+//object 
+
+//typeof undefined #udefined 
+//typeof null #object 
