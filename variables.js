3c3a1a098a35be80950d32299c693cb20a8edaad
manojgarnayak <manojgarnayak07@gmail.com>
2023-09-11 15:07:02 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 134567 //cannot be changed or should not be changed 
+
+let accountEmail = "manojgarnayak@gmail.com"
+
+var accountPassword = "12345" //nowadays we do not use var it shows problem during scope block and functional scope
+
+accountCity = "Rourkela" //we can declare like this but it is not recommended 
+
+let accountState
+
+console.table([accountId, accountPassword, accountEmail, accountState,accountCity])
