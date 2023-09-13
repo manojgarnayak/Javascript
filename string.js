5f0a8f11acaae63497c3c5f90af064f5977736d5
manojgarnayak <manojgarnayak07@gmail.com>
2023-09-13 15:57:57 +0530

    String file

---
+++ string.js
@@ -0,0 +1,8 @@
+const name = "Manoj"
+const repoCount = 5
+
+//console.log(name + " " + repoCount )
+
+console.log(`My name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('Manoj Garnayak')  //declaration
