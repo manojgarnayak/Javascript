47e422abfe6d2556c777f6a4f8435172ed5eba92
manojgarnayak <manojgarnayak07@gmail.com>
2024-01-04 11:24:20 +0530

    Destructuring and JSON intro

---
+++ json.js
@@ -0,0 +1,18 @@
+//Destructuring of Objects
+
+const course = {
+    cname: "Js Hindi",
+    price: 199,
+    cinstructor: "Hitesh"
+}
+const {cinstructor: teacher} = course //destructuring
+console.log(teacher);
+
+//JSON
+
+// {
+//     "name": "Hitesh",
+//     "cname": "Js in hindi",
+//     "price": "free"
+// }
+
