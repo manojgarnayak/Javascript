76fd2fe601d634801f259bdeed3bb04eafac2a1b
manojgarnayak <manojgarnayak07@gmail.com>
2024-01-07 10:48:41 +0530

    While loop and do while loop

---
+++ while.js
@@ -0,0 +1,23 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    //console.log(`value of index is ${index}`);
+    index++
+}
+
+
+let arr = ["batman","superman","flash"]
+let i = 0
+while (i < arr.length) {
+    //console.log(`superhero is ${arr[i]}`)
+    i++
+}
+
+// do While Loop
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
