2d074feacdaa15370507c23a7257f85da2f32a58
manojgarnayak <manojgarnayak07@gmail.com>
2024-01-09 10:28:29 +0530

    Theme changer project

---
+++ Projects/Themechanger/project1.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
